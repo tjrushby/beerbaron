4522b1d2cde3a1fec0e5782230235bec1c1cccaa
Tim Rushby <tjrushby@outlook.com>
2017-03-27 12:24:45 +1100

    Added database trigger for calculating the average price of a product

--- main/src/SQLiteInitializer.java
+++ main/src/SQLiteInitializer.java
@@ -27,14 +27,14 @@ public class SQLiteInitializer {
     private static final String PRICECHECK_COLUMN_PRICE = "pc_price";
 
     // queries for table creation
-    private static final String DATABASE_CREATE_PRODUCT = "CREATE TABLE IF NOT EXISTS " +
+    private static final String DATABASE_CREATE_TABLE_PRODUCT = "CREATE TABLE IF NOT EXISTS " +
             TABLE_PRODUCT + "(" +
                 PRODUCT_COLUMN_ID + " TEXT NOT NULL, " +
                 PRODUCT_COLUMN_NAME + " TEXT, " +
                 PRODUCT_COLUMN_AVGPRICE + " REAL, " +
             "CONSTRAINT Product_p_id_pk PRIMARY KEY (" + PRODUCT_COLUMN_ID + "));";
 
-    private static final String DATABASE_CREATE_PRICECHECK = "CREATE TABLE IF NOT EXISTS " +
+    private static final String DATABASE_CREATE_TABLE_PRICECHECK = "CREATE TABLE IF NOT EXISTS " +
             TABLE_PRICECHECK + "(" +
                 PRODUCT_COLUMN_ID + " TEXT NOT NULL, " +
                 PRICECHECK_COLUMN_DATETIME + " INTEGER NOT NULL, " +
@@ -43,22 +43,38 @@ public class SQLiteInitializer {
             "CONSTRAINT PriceCheck_p_id FOREIGN KEY (" + PRODUCT_COLUMN_ID +
             ") REFERENCES " + TABLE_PRODUCT + "(" + PRODUCT_COLUMN_ID + "));";
 
+    // queries for trigger creation
+    private static final String DATABASE_CREATE_TRIGGER_AVGPRICE =
+            "CREATE TRIGGER 'update_product_avgprice' " +
+                "AFTER INSERT ON '" + TABLE_PRICECHECK + "' " +
+                "FOR EACH ROW " +
+            "BEGIN " +
+                "UPDATE '" + TABLE_PRODUCT + "' " +
+                "SET " + PRODUCT_COLUMN_AVGPRICE + " = (" +
+                    "SELECT AVG(" + TABLE_PRICECHECK + "." + PRICECHECK_COLUMN_PRICE + ") " +
+                    "FROM '" + TABLE_PRICECHECK + "' " +
+                    "WHERE " + TABLE_PRICECHECK + "." + PRODUCT_COLUMN_ID + " = " + TABLE_PRODUCT + "." + PRODUCT_COLUMN_ID + " " +
+                ");" +
+            "END;";
+
     public SQLiteInitializer() {
         // check if there is an existing database
         if(!Files.exists(Paths.get(DATABASE_NAME))) {
-            // there isn't an existing database so we need to create the tables and add default products
-            createTables();
+            // there isn't an existing database so we need to create the tables, triggers and add default products
+            createDatabaseStructure();
             addDefaultProducts();
         }
     }
 
-    public void createTables() {
+    // creates the tables and triggers in the database
+    public void createDatabaseStructure() {
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
 
             Statement statement = connection.createStatement();
-            statement.execute(DATABASE_CREATE_PRODUCT);
-            statement.execute(DATABASE_CREATE_PRICECHECK);
+            statement.execute(DATABASE_CREATE_TABLE_PRODUCT);
+            statement.execute(DATABASE_CREATE_TABLE_PRICECHECK);
+            statement.execute(DATABASE_CREATE_TRIGGER_AVGPRICE);
             statement.close();
 
             connection.close();
