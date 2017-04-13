7e8feb644567843ee3be963d674c792902d13ca7
Tim Rushby <tjrushby@outlook.com>
2017-04-13 14:08:55 +1000

    Added the ability to perform a price check under the File menu

--- main/src/BeerBaronController.java
+++ main/src/BeerBaronController.java
@@ -2,18 +2,27 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BeerBaronController implements Initializable {
     @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private MenuItem menuItemCheckPrices;
+
+    @FXML
     private ListView<Product> listView;
 
     private ObservableList<Product> products;
+    private SQLiteInitializer sqLiteInitializer;
 
     public BeerBaronController() {
-        SQLiteInitializer sqLiteInitializer = new SQLiteInitializer();
+        sqLiteInitializer = new SQLiteInitializer();
         products = sqLiteInitializer.getAllProducts();
     }
 
@@ -25,5 +34,14 @@ public class BeerBaronController implements Initializable {
 
         // tell the CellFactory to use ProductListCell instead of the default
         listView.setCellFactory(productListView -> new ProductListCell());
+
+        // set the on action methods for the menu items
+        menuItemCheckPrices.setOnAction(e -> {
+            // perform a price check
+            sqLiteInitializer.addPriceCheck();
+
+            // update listView with the new prices from the price check
+            listView.setItems(sqLiteInitializer.getAllProducts());
+        });
     }
 }
